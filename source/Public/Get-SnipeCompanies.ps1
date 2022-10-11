a4750620cf13e80a81b655d05ff9b9c4d09e771f
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-10-11 15:29:18 +0200

    Create Get-SnipeCompanies.ps1

---
+++ source/Public/Get-SnipeCompanies.ps1
@@ -0,0 +1,49 @@
+function Get-SnipeCompanies {
+    [CmdletBinding(DefaultParameterSetName="List companies")]
+    param (
+        [Parameter(Position=0,ParameterSetName="List companies")][string]$name,
+        [Parameter(Mandatory,ParameterSetName="Get company by id")][int]$id
+    )
+
+    $uri = "$(Get-SnipeEndpoint)/companies"
+
+    switch ($PsCmdlet.ParameterSetName) {
+        "List companies" {
+            $PSBoundParameters.Keys.ForEach({
+                $key = $_
+                $value = $PSBoundParameters.$key
+
+                if (([array]$PSBoundParameters.Keys)[0] -eq $key) {
+                    $delimiter = "?"
+                } else {
+                    $delimiter = "&"
+                }
+
+                $uri = "$uri$delimiter$key=$value"
+            })
+        }
+        "Get company by id" {
+            $uri = "$uri/$id"
+        }
+    }
+
+    $splat = @{
+        "Uri" = $uri
+        "Method" = "GET"
+        "Headers" = @{
+            "Authorization" = "Bearer $(Get-SnipeAccessToken)"
+            "Accept" = "application/json"
+            "Content-Type" = "application/json"
+        }
+    }
+    $result = Invoke-RestMethod @splat
+    
+    switch ($PsCmdlet.ParameterSetName) {
+        "List companies" {
+            $result.rows
+        }
+        "Get company by id" {
+            $result
+        }
+    }
+}
