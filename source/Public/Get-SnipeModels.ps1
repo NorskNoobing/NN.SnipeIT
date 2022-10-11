bcb247f81b22fbee9a6e64b98c222749e7744af3
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-10-11 15:29:29 +0200

    Create Get-SnipeModels.ps1

---
+++ source/Public/Get-SnipeModels.ps1
@@ -0,0 +1,53 @@
+function Get-SnipeModels {
+    [CmdletBinding(DefaultParameterSetName="List models")]
+    param (
+        [Parameter(Position=0,ParameterSetName="List models")][string]$search,
+        [Parameter(ParameterSetName="List models")][int]$limit,
+        [Parameter(ParameterSetName="List models")][int]$offset,
+        [Parameter(ParameterSetName="List models")][string]$sort,
+        [Parameter(ParameterSetName="List models")][ValidateSet("asc","desc")][int]$order,
+        [Parameter(Mandatory,ParameterSetName="Get model by id")][int]$id
+    )
+
+    $uri = "$(Get-SnipeEndpoint)/models"
+
+    switch ($PsCmdlet.ParameterSetName) {
+        "List models" {
+            $PSBoundParameters.Keys.ForEach({
+                $key = $_
+                $value = $PSBoundParameters.$key
+
+                if (([array]$PSBoundParameters.Keys)[0] -eq $key) {
+                    $delimiter = "?"
+                } else {
+                    $delimiter = "&"
+                }
+
+                $uri = "$uri$delimiter$key=$value"
+            })
+        }
+        "Get model by id" {
+            $uri = "$uri/$id"
+        }
+    }
+
+    $splat = @{
+        "Uri" = $uri
+        "Method" = "GET"
+        "Headers" = @{
+            "Authorization" = "Bearer $(Get-SnipeAccessToken)"
+            "Accept" = "application/json"
+            "Content-Type" = "application/json"
+        }
+    }
+    $result = Invoke-RestMethod @splat
+    
+    switch ($PsCmdlet.ParameterSetName) {
+        "List models" {
+            $result.rows
+        }
+        "Get model by id" {
+            $result
+        }
+    }
+}
