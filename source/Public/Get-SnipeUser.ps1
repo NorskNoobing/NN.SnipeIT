24f64be82c233e3b4851ab7122778a5c3fe248a2
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-10-11 09:14:17 +0200

    Create Get-SnipeUser.ps1

---
+++ source/Public/Get-SnipeUser.ps1
@@ -0,0 +1,73 @@
+function Get-SnipeUser {
+    [CmdletBinding(DefaultParameterSetName="List users")]
+    param (
+        [Parameter(Position=0,ParameterSetName="List users")][string]$search,
+        [Parameter(ParameterSetName="List users")][int]$limit,
+        [Parameter(ParameterSetName="List users")][int]$offset,
+        [Parameter(ParameterSetName="List users")][string]$sort,
+        [Parameter(ParameterSetName="List users")][ValidateSet("asc","desc")][string]$order,
+        [Parameter(ParameterSetName="List users")][string]$first_name,
+        [Parameter(ParameterSetName="List users")][string]$last_name,
+        [Parameter(ParameterSetName="List users")][string]$username,
+        [Parameter(ParameterSetName="List users")][string]$email,
+        [Parameter(ParameterSetName="List users")][string]$employee_num,
+        [Parameter(ParameterSetName="List users")][string]$state,
+        [Parameter(ParameterSetName="List users")][string]$zip,
+        [Parameter(ParameterSetName="List users")][string]$country,
+        [Parameter(ParameterSetName="List users")][int]$group_id,
+        [Parameter(ParameterSetName="List users")][int]$department_id,
+        [Parameter(ParameterSetName="List users")][int]$company_id,
+        [Parameter(ParameterSetName="List users")][int]$location_id,
+        [Parameter(ParameterSetName="List users")][ValidateSet("true","false")][string]$deleted,
+        [Parameter(ParameterSetName="List users")][ValidateSet("true","false")][string]$all,
+        [Parameter(ParameterSetName="List users")][ValidateSet("0","1")][int]$ldap_import,
+        [Parameter(ParameterSetName="List users")][int]$asset_count,
+        [Parameter(ParameterSetName="List users")][int]$licenses_count,
+        [Parameter(ParameterSetName="List users")][int]$accessories_count,
+        [Parameter(ParameterSetName="List users")][int]$consumables_count,
+        [Parameter(ParameterSetName="List users")][ValidateSet("0","1")][int]$remote,
+        [Parameter(Mandatory,ParameterSetName="Get user by id")][int]$id
+    )
+
+    $uri = "$(Get-SnipeEndpoint)/users"
+
+    switch ($PsCmdlet.ParameterSetName) {
+        "List users" {
+            $PSBoundParameters.Keys.ForEach({
+                $key = $_
+                $value = $PSBoundParameters.$key
+
+                if (([array]$PSBoundParameters.Keys)[0] -eq $key) {
+                    $delimiter = "?"
+                } else {
+                    $delimiter = "&"
+                }
+
+                $uri = "$uri$delimiter$key=$value"
+            })
+        }
+        "Get user by id" {
+            $uri = "$uri/$id"
+        }
+    }
+
+    $splat = @{
+        "Uri" = $uri
+        "Method" = "GET"
+        "Headers" = @{
+            "Authorization" = "Bearer $(Get-SnipeAccessToken)"
+            "Accept" = "application/json"
+            "Content-Type" = "application/json"
+        }
+    }
+    $result = Invoke-RestMethod @splat
+    
+    switch ($PsCmdlet.ParameterSetName) {
+        "List users" {
+            $result.rows
+        }
+        "Get user by id" {
+            $result
+        }
+    }
+}
